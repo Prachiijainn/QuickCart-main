2d712e5c87674eb12fb4e1ad3ca9b75f6b40bdf8
prachiijainn <prachiijainn1@gmail.com>
2025-05-16 20:08:28 +0530

    made change

--- app/api/product/list/route.js
+++ app/api/product/list/route.js
@@ -1,24 +1,24 @@
 import connectDB from "../../../../config/db";
+import authSeller from "../../../../lib/authSeller";
 import Product from "../../../../models/product";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
-  try {
-    await connectDB();
-    
-    // Get all products
-    const products = await Product.find({});
-    
-    return NextResponse.json({
-      success: true,
-      message: 'Products retrieved successfully',
-      products
-    });
-  } catch (error) {
-    console.error('Error in product list API:', error);
-    return NextResponse.json(
-      { success: false, message: error.message },
-      { status: 500 }
-    );
-  }
-}
+    try {
+       
+
+        await connectDB()
+
+        // Get products for this seller only
+        const products = await Product.find({ userId })
+        
+        return NextResponse.json({ 
+            success: true, 
+            message: 'Products retrieved successfully',
+            products
+        });
+    } catch (error) {
+        console.error('Error in seller-list API:', error)
+        return NextResponse.json({ success: false, message: error.message }, { status: 500 })
+    }
+} 
